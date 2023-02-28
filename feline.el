dd28af7aa42fc00477e993d3b0dc28678626b239
chee <yay@chee.party>
2023-02-28 01:31:22 +0000

    Clean up package header and function naming scheme

--- feline.el
+++ feline.el
@@ -1,20 +1,48 @@
-;;; feline --- a modeline with very little
+;;; feline.el --- A modeline with very little
+
+;; Author: chee <emacs@chee.party>
+;; Homepage: https://opensource.chee.party/chee/feline-mode
+;; Version: 1
+;; Package-Requires: ((emacs "28.1"))
+
 ;;; Commentary:
-;;; be fast and small
+;; feline is a fast, low-frills modeline replacement for Emacs.
+
+;; feline saves space by displaying only a few things:
+;;   • evil state
+;;     (state name and customizable colour)
+;;   • file-edited state
+;;     (a * if edited)
+;;   • the major mode
+;;     (or a customizable shorthand/symbol alias)
+;;   • buffer ID
+;;     (e.g. filename)
+;;   • the current project.el project name
+;;     (if active)
+;;   • line and column number
+;;     (like L18C40)
+;;   • `mode-line-misc-info'
+;;     (a place that modes put useful info sometimes)
+
+;; Each segment has its own customizable face.  You'll notice that list does not
+;; mention minor modes.  feline doesn't show minor modes.  The information it
+;; chooses to show is very useful, and being limited like this lets it be very
+;; fast and nimble.
+
 ;;; Code:
 
 (defgroup feline nil
   "The feline modeline."
   :group 'mode-line
   :link '(url-link
-           :tag "Homepage" "https://git.sr.ht/~chee/feline-mode"))
+           :tag "Homepage" "https://opensource.chee.party/chee/feline-mode"))
 
 (defgroup feline-faces nil
   "Feline faces."
   :group 'feline
   :group 'faces
   :link '(url-link
-           :tag "Homepage" "https://git.sr.ht/~chee/feline-mode"))
+           :tag "Homepage" "https://opensource.chee.party/chee/feline-mode"))
 
 (defface feline-buffer-id-face
   '((t ()))
@@ -28,12 +56,12 @@
 
 (defface feline-position-prefix-face
   '((t (:inherit font-lock-comment-face)))
-  "The prefix for line and column positions."
+  "The prefix for line and column positions in the feline."
   :group 'feline-faces)
 
 (defface feline-project-name-face
   '((t (:weight bold)))
-  "The name of the current project."
+  "The name of the current project in the feline."
   :group 'feline-faces)
 
 (defface feline-evil-face
@@ -94,24 +122,24 @@ If there is no match, the fallback is the mode name without the -mode suffix."
   :type '(plist :key-type symbol :value-type string)
   :group 'feline)
 
-(defun feline/get-mode-symbol (mode)
-  "Get an symbol representation for MODE from the list `feline/mode-symbol'."
+(defun feline-get-mode-symbol (mode)
+  "Get an symbol representation for MODE from the list `feline-mode-symbol'."
   (propertize
 	 (or
 		(plist-get
-		  feline-mode-symbols major-mode)
-		(format "%s" major-mode))
+		  feline-mode-symbols mode)
+		(format "%s" mode))
 	 'face 'feline-major-mode-face))
 
-(defun feline/mode-name (mode)
+(defun feline-mode-name (mode)
   "Get a feline representation of MODE."
-  (replace-regexp-in-string "-mode$" "" (feline/get-mode-symbol mode)))
+  (replace-regexp-in-string "-mode$" "" (feline-get-mode-symbol mode)))
 
-(defun feline/major-mode nil
+(defun feline-major-mode nil
   "Get a feline representation of the major mode."
-  (feline/mode-name major-mode))
+  (feline-mode-name major-mode))
 
-(defun feline/buffer-id (buffer-id)
+(defun feline-buffer-id (buffer-id)
   "Get a feline representation of BUFFER-ID."
   (propertize
 	 (replace-regexp-in-string
@@ -120,7 +148,7 @@ If there is no match, the fallback is the mode name without the -mode suffix."
 		buffer-id)
 	 'face 'feline-buffer-id-face))
 
-(defun feline/evil nil
+(defun feline-evil nil
   "Get a feline representation of the evil mode."
   (when (bound-and-true-p evil-local-mode)
 	 (apply 'propertize
@@ -134,7 +162,7 @@ If there is no match, the fallback is the mode name without the -mode suffix."
 		  ((evil-replace-state-p) '("replace" face feline-evil-replace-face))
 		  (t ("normal" 'face 'feline-evil-normal-face))))))
 
-(defun feline/positions nil
+(defun feline-positions nil
   "Present the line and column in the feline."
   (concat (propertize
 		      feline-line-prefix
@@ -151,21 +179,21 @@ If there is no match, the fallback is the mode name without the -mode suffix."
 
 (defvar feline--project-name-cache '())
 
-(defun feline/--last-meaningful-string (list)
+(defun feline--last-meaningful-string (list)
   "Get last meaningful string from LIST."
   (let* ((n (car list)) (rest (cdr list)) (nn (car rest)))
 	 (if (or (not nn) (string-empty-p nn))
 	   n
-		(feline/--last-meaningful-string rest))))
+		(feline--last-meaningful-string rest))))
 
-(defun feline/--project-name nil
+(defun feline--project-name nil
   "Get the current project, if we're in one."
   (let ((project (project-current)))
 	 (when project
-		(feline/--last-meaningful-string
+		(feline--last-meaningful-string
 		  (split-string (project-root project) "/")))))
 
-(defun feline/project-name nil
+(defun feline-project-name nil
   "Get a feline representation of the current project name."
   (let* ((filename (buffer-file-name))
 			 (cached (plist-get feline--project-name-cache filename)))
@@ -176,49 +204,51 @@ If there is no match, the fallback is the mode name without the -mode suffix."
 		  (if (and (fboundp 'tramp-tramp-file-p) (tramp-tramp-file-p filename))
 			 "TRAMP"
 	       (if cached cached
-			   (or (feline/--project-name) "")))
+			   (or (feline--project-name) "")))
 		  'mouse-face 'mode-line-highlight
 		  'local-map (let ((🐈‍⬛ (make-sparse-keymap)))
 			            (define-key 🐈‍⬛ [mode-line mouse-1] 'project-switch-project)
 			            🐈‍⬛)
 		  'face 'feline-project-name-face))))
 
-(defvar feline/original-modeline mode-line-format)
+(defvar feline-original-modeline mode-line-format)
 
 (defvar feline-mode nil "\"t\" if feline is active.")
 
-(defun feline/spray nil
+(defun feline-spray nil
   "Stop feline, returning the previous modeline."
   (setq feline-mode nil)
-  (setq-default mode-line-format feline/original-modeline))
+  (setq-default mode-line-format feline-original-modeline))
 
-(defun feline/purr nil
+(defun feline-purr nil
   "Start feline."
   (setq feline-mode t)
   (setq-default
 	 mode-line-format
 	 '(""
-		 (:eval (feline/evil))
+		 (:eval (feline-evil))
 		 " "
 		 (:eval (propertize (if (buffer-modified-p) "*" "") 'face 'feline-buffer-id-face))
-		 (:eval (feline/major-mode))
+		 (:eval (feline-major-mode))
 		 " "
-		 (:eval (feline/buffer-id (format-mode-line "%b")))
+		 (:eval (feline-buffer-id (format-mode-line "%b")))
 		 " "
-		 (:eval (feline/project-name))
+		 (:eval (feline-project-name))
 		 " "
-		 (:eval (feline/positions))
+		 (:eval (feline-positions))
 		 " "
 		 mode-line-misc-info)))
 
+;;;###autoload
 (define-minor-mode feline-mode
   "Toggle _f_e_l_i_n_e_."
   :group 'feline
   :global t
   :lighter nil
   (if feline-mode
-	 (feline/purr)
-	 (feline/spray)))
+	 (feline-purr)
+	 (feline-spray)))
 
 (provide 'feline)
-;;; feline ends here
+
+;;; feline.el ends here
